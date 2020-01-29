ec8c13cff3b5686b798528398c2b0622fea5ee50
unknown <silva.gabrielmiranda@gmail.com>
2020-01-29 19:41:45 -0300

    Adicionado botão adicionar tarefa

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import './styles.css';
 
 export default class Item extends Component {
-    handleChange(event){
-        console.log("Mudança - Checkbox");
-    }
     render() {
 
         const dados = this.props.dados;
-
+        console.log(dados);
         return (
             <div className="item">
                 <h1>{dados.titulo}</h1>
                 <p>{dados.descricao}</p>
                 <ul>
-                    { dados.items.map(item => <li><input type="checkbox" onChange={this.handleChange}/>{item}</li>) }
+                    {dados.items.map(item => <li>{item}</li>)}
                 </ul>
+                <div className="right-button">
+                    <button>+</button>
+                </div>
             </div>
         )
     }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,7 @@ import Item from '../Item';
 
 export default class List extends Component {
     state = {
-        item: { titulo: "Criar APP em React", descricao: "Criar um aplicativo utilizando React, um framework de javascipt", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] },
+        item: { _id: 1, titulo: "Criar APP em React", descricao: "Criar um aplicativo utilizando React, um framework de javascipt", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] },
     };
     render() {
         return (
