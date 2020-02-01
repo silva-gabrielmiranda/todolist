1b9e18ae6b5283555c1c5850f4295f8eac6c955a
unknown <silva.gabrielmiranda@gmail.com>
2020-02-01 20:28:29 -0300

    add novo item na lista

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -3,6 +3,25 @@ import React, { Component } from 'react';
 import './styles.css';
 
 export default class Item extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            novoItemList: "",
+        }
+        this.addItem = this.addItem.bind(this);
+        this.inputHandle = this.inputHandle.bind(this);
+    }
+
+    addItem(){
+        this.props.addItem({ "_id": this.props.dados._id, "titulo": this.props.dados.titulo, "descricao": this.props.dados.descricao, "items": this.props.dados.items.concat(this.state.novoItemList) }, this.props.dados._id)
+    }
+    inputHandle(event){
+        this.setState({ novoItemList: event.target.value });
+        console.log(this.state.novoItemList);
+        console.log(event)
+    }
+
     render() {
 
         const dados = this.props.dados;
@@ -13,8 +32,9 @@ export default class Item extends Component {
                 <ul>
                     {dados.items.map(item => <li>{item}</li>)}
                 </ul>
+                <input onChange={this.inputHandle}></input>
                 <div className="right-button">
-                    <button>+</button>
+                    <button onClick={this.addItem}>+</button>
                 </div>
             </div>
         )
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,40 +6,43 @@ export default class List extends Component {
         super(props);
         this.state = {
             item: [
-                { _id: 1, titulo: "Criar APP em Javascript", descricao: "Criar um aplicativo utilizando React, um framework de javascipt", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] },
-                { _id: 2, titulo: "Criar APP em React", descricao: "Criar um aplicativo utilizando React, um framework de javascipt", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] }
+                { _id: 1, titulo: "Javascript", descricao: "Linguagem de programação", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] },
+                { _id: 2, titulo: "React", descricao: "Framework do javascript", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] }
             ],
             newItem: {},
         };
         this.createNewItem = this.createNewItem.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.verificaEstado = this.verificaEstado.bind(this);
+        this.addItemLista = this.addItemLista.bind(this);
     }
 
     createNewItem() {
-        // let newItem = { _id: 3, titulo: "Criar APP em React Native", descricao: "Criar um aplicativo utilizando React, um framework de javascipt", items: ["Aprender Javacript Nativo", "Aprender ES6", "Criar um aplicativo de lista"] };
         let newItem = this.state.item.concat(this.state.newItem);
-        this.setState({item: newItem});
-        console.log("State: ");
-        console.log(this.state.item);
-        // console.log("Item: ");
-        // console.log(item);
-        console.log("newItem: ");
-        console.log(this.state.newItem);
+        this.setState({ item: newItem });
     }
 
-    handleChange(event){
-        this.setState({newItem: JSON.parse(event.target.value)});
+    handleChange(event) {
+        this.setState({ newItem: JSON.parse(event.target.value) });
         console.log(this.state.newItem);
     }
-    verificaEstado(){
+    verificaEstado() {
         console.log("Verifica state", this.state)
+    }
+    addItemLista(novoItem, id) {
+        console.log("Objeto: ", novoItem);
+        console.log("id", id);
+        let oldItem = this.state.item.filter(item => item._id !== id).concat(novoItem);
+        console.log("Item filtrado pelo ID: " + id, oldItem);
+        console.log("novoItem após alteração" + id, novoItem);
+        oldItem.sort((a,b) => a - b);
+        this.setState({item: oldItem});
     }
 
     render() {
         return (
             <div>
-                {this.state.item.map(bloco => <Item dados={bloco} />)}
+                {this.state.item.map(bloco => <Item dados={bloco} addItem={this.addItemLista} />)}
                 <input onChange={this.handleChange} />
                 <button onClick={this.createNewItem}>Adicionar</button>
                 <button onClick={this.verificaEstado}>Verifica State</button>
